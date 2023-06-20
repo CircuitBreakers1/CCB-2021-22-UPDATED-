f15911a30b19be49630710eff1756a8578844940
Lucas <lqi20@yahoo.com>
2023-06-19 20:52:35 -0500

    Summer Main Robot Complete

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,128 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.SummerJohn2023.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TeleOP")
+public class TeleOP extends LinearOpMode {
+    SummerJohn2023 robot = new SummerJohn2023();
+
+    //Constants
+    int armLevels[] = {500, 1000, 1500};
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        rotate.setPower(0.5);
+        while(rotateTrigger.getState()) {
+            telemetry.addData("Status", "Zeroing...");
+            telemetry.update();
+        }
+        rotate.setPower(0);
+        rotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rotate.setTargetPosition(0);
+
+        rotate.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+//        leftLift.setTargetPosition(0);
+//        leftLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        robot.setArmHeight(0);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Set the first gamepad led to purple, and the second to yellow
+        gamepad1.setLedColor(255, 0, 255, -1);
+        gamepad2.setLedColor(255, 255, 0, -1);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            double heading = 69;
+
+            double x_rotated = x * Math.cos(heading) - y * Math.sin(heading);
+            double y_rotated = x * Math.sin(heading) + y * Math.cos(heading);
+
+            leftFront.setPower(1 * (y + x + rx));
+            leftBack.setPower(1 * (y - x + rx));
+            rightFront.setPower(1 * (y - x - rx));
+            rightBack.setPower(1 * (y + x - rx));
+
+            rotate.setPower(-gamepad2.left_stick_x * 0.5);
+
+
+            if(gamepad2.a) {
+                robot.setArmHeight(armLevels[0]);
+            }
+            if(gamepad2.b) {
+                robot.setArmHeight(armLevels[1]);
+            }
+            if(gamepad2.y) {
+                robot.setArmHeight(armLevels[2]);
+            }
+            if(gamepad2.x) {
+                robot.setArmHeight(0);
+            }
+//            leftLift.setPower(-gamepad2.left_stick_y);
+//            rightLift.setPower(gamepad2.left_stick_y);
+
+            if(gamepad2.left_bumper) {
+                grab.setPosition(0);
+            }
+            if(gamepad2.right_bumper) {
+                grab.setPosition(1);
+            }
+
+            robot.maintainArm();
+
+            telemetry.addData("Left Arm Speed", leftLift.getPower());
+            telemetry.addData("Right Arm Speed", rightLift.getPower());
+            telemetry.addData("Turret Location", rotate.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
